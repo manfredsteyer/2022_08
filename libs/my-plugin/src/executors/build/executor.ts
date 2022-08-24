cf618711f653d5b537a4e8de5af4e5ff8c5662c8
Manfred Steyer <manfred.steyer@gmx.net>
2022-08-24 15:05:11 +0200

    b4 generator

--- libs/my-plugin/src/executors/build/executor.ts
+++ libs/my-plugin/src/executors/build/executor.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
   console.log('Executor ran for Build', options);
-  
+
   fs.writeFileSync(`${options.folder}/demo.txt`, 'Demo Task!');
 
   return {
